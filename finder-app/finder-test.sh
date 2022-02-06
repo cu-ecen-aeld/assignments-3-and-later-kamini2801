5ead44d7bce5387504621e51dc9dcb85ce8360b1
kamini2801 <kamini.budke@colorado.edu>
2022-02-06 15:43:29 -0700

    chore: conf path updated

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,7 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 FINDERDIR=/tmp
-username=$(cat ./conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 2 ]
 then
