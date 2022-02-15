ee7ede3593a6c3bb17542069d6d27972fd8348e0
kamini2801 <kamini.budke@colorado.edu>
2022-02-14 17:09:04 -0700

    feat: updated to pass socket_test

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdbool.h>
+#include <signal.h>
 
 #define MY_SOCK_PATH "/var/tmp/aesddata"
 #define LISTEN_BACKLOG 50
@@ -24,13 +25,6 @@
 #define BUF_SIZE 1024
 #define TRUE 1
 #define FALSE 0
-
-// #define handle_error(msg)   \
-//     do                      \
-//     {                       \
-//         perror(msg);        \
-//         exit(EXIT_FAILURE); \
-//     } while (0)
 
 int main(int argc, char *argv[])
 {
@@ -64,31 +58,13 @@ int main(int argc, char *argv[])
 
     if (bind(sfd, serverinfo->ai_addr,
              sizeof(server_addr)) == -1)
-    {       
+    {
         perror("bind");
         exit(-1);
     }
     printf("Bound scoket\n");
 
-    if (listen(sfd, LISTEN_BACKLOG) == -1)
-        perror("listen");
-
-    /* Now we can accept incoming connections one
-              at a time using accept(2). */
-
-    client_addr_size = sizeof(client_addr);
-    cfd = accept(sfd, (struct sockaddr *)&client_addr,
-                 &client_addr_size);
-    if (cfd == -1)
-        perror("accept");
-    printf("Accepted connection\n");
-
-    /* 
-    *   Code to deal with incoming connection(s)... 
-    *
-    * 
-    * */
-   remove(MY_SOCK_PATH);
+    remove(MY_SOCK_PATH);
 
     fd = open(MY_SOCK_PATH, O_CREAT | O_RDWR | O_APPEND, 0644);
     if (fd == -1)
@@ -97,88 +73,121 @@ int main(int argc, char *argv[])
     }
     printf("Opened file\n");
 
-    memset(&recv_buf, '\0', BUF_SIZE);
-
-    //Extracting a packet
-    int i;
-    int recv_len = 0;
-    int recv_complete = FALSE;
-    int first_cycle = TRUE;
-    char *tx_buf;
-
-    while (!recv_complete)
+    while (1)
     {
-        int prev_size=0;
+        if (listen(sfd, LISTEN_BACKLOG) == -1)
+            perror("listen");
 
-       if(recv(cfd, &recv_buf, BUF_SIZE, 0)==-1)
-       {
-           syslog(LOG_DEBUG, "Connection clsoed\n");
-            break;
-       }
+        /* Now we can accept incoming connections one
+              at a time using accept(2). */
 
-        for (i = 0; i < BUF_SIZE; i++)
-        {   
-            recv_len++;
+        client_addr_size = sizeof(client_addr);
+        cfd = accept(sfd, (struct sockaddr *)&client_addr,
+                     &client_addr_size);
+        if (cfd == -1)
+            perror("accept");
+        printf("Accepted connection\n");
 
-            if (recv_buf[i] == '\n')
+        /* 
+    *   Code to deal with incoming connection(s)... 
+    *
+    * 
+    * */
+
+        memset(&recv_buf, '\0', BUF_SIZE);
+
+        //Extracting a packet
+        int i;
+        int recv_len = 0;
+        int recv_complete = FALSE;
+        int first_cycle = TRUE;
+        char *tx_buf, *temp_ptr;
+
+        while (!recv_complete)
+        {
+            int prev_size = 0;
+
+            if (recv(cfd, &recv_buf, BUF_SIZE, 0) == -1)
             {
-                recv_complete=TRUE;
+                syslog(LOG_DEBUG, "Connection clsoed\n");
                 break;
+            }
+
+            for (i = 0; i < BUF_SIZE; i++)
+            {
+                recv_len++;
+
+                if (recv_buf[i] == '\n')
+                {
+                    recv_complete = TRUE;
+                    break;
+                }
+            }
+
+            if (first_cycle)
+            {
+                tx_buf = (char *)malloc(recv_len);
+
+                memset(tx_buf, '\0', recv_len);
+
+                first_cycle = FALSE;
+            }
+            else
+            {
+                prev_size += recv_len;
+                //printf("pREV_SIZE: %d\n", prev_size);
+                if ((temp_ptr = (char *)realloc(tx_buf, prev_size)) == NULL)
+                {
+                    perror("realloc");
+                    exit(-1);
+                }
+                else
+                {
+                    tx_buf = temp_ptr;
+                }
+            }
+
+            strncat(tx_buf, recv_buf, recv_len); //copy present contents
+        }
+
+        printf("Write packet to file\n");
+
+        //Appending Packet to the file
+        lseek(fd, 0, SEEK_END);
+        int bytes = write(fd, tx_buf, recv_len);
+        if (bytes == -1)
+        {
+            syslog(LOG_ERR, "Could not write to file\n");
+            return -1;
+        }
+        printf("Writing %d bytes\n", bytes);
+
+        lseek(fd, 0, SEEK_SET);
+
+        while ((bytes = read(fd, recv_buf, BUF_SIZE)) != 0)
+        {
+            //bytes = read(fd, recv_buf, BUF_SIZE);
+            if (bytes == -1)
+            {
+                syslog(LOG_ERR, "Could not read to file\n");
+                return -1;
+            }
+            printf("Reading %d bytes\n", bytes);
+            if (send(cfd, &recv_buf, bytes, 0) == -1)
+            {
+                syslog(LOG_DEBUG, "Connection clsoed\n");
             }
         }
 
-        if(first_cycle)
-        {
-            tx_buf = (char*)malloc(recv_len);
-
-            first_cycle=FALSE; 
-        }
-        else
-        {   
-            prev_size+= recv_len;
-            
-            tx_buf= (char*)realloc(tx_buf, recv_len); 
-
-            if(tx_buf==NULL)    
-                perror("realloc");
-        }
-
-        strncpy(tx_buf + prev_size, recv_buf, recv_len);    //copy present contents
+        free(tx_buf);
+        printf("end\n");
     }
 
-    printf("Write packet to file\n");
-
-    //Appending Packet to the file
-    int bytes = write(fd, tx_buf, recv_len);
-    if (bytes == -1)
-    {
-        syslog(LOG_ERR, "Could not write to file\n");
-        return -1;
-    }
-     printf("Writing %d bytes\n", bytes );
-
-    lseek(fd, 0, SEEK_SET);
-    bytes = read(fd, recv_buf, BUF_SIZE);
-    if (bytes == -1)
-    {
-        syslog(LOG_ERR, "Could not read to file\n");
-        return -1;
-    }
-    printf("Reading %d bytes\n", bytes );
-    if(send(cfd, &recv_buf, bytes, 0)==-1)
-    {
-        syslog(LOG_DEBUG, "Connection clsoed\n");
-
-    }
     freeaddrinfo(serverinfo);
 
     close(sfd);
     close(cfd);
     close(fd);
-
-
-    printf("end\n");
-
     /* When no longer required, the socket pathname, MY_SOCK_PATH
               should be deleted using unlink(2) or remove(3). */
 }
